86abe27b682b4bcb41417494b5fd23ab805d6705
Frowcaloe <forgame061601@gmail.com>
2024-09-11 07:44:03 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I hope you are doing well'.split('')
+const text = 'Thank you pu'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
